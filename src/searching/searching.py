33ba18c3c116edd2afd9ca9c07c22d0c876b4fe4
Reagan Barrington <reagan@barringtonfamily.net>
2020-07-21 16:46:57 -0400

    mvp

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -3,7 +3,7 @@ import math
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
     # if empty, return -1
-    if not arr:
+    if start > end:
         return -1
 
     middle = round((start + end) / 2)
@@ -29,6 +29,21 @@ def binary_search(arr, target, start, end):
 # sorted in ascending order or in descending order
 # You can implement this function either recursively 
 # or iteratively
+
+# iterative
 def agnostic_binary_search(arr, target):
     # Your code here
-    pass
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+    return -1 
+
+
+# recursively
+# def agnostic_binary_search(arr, target):
+#     i = 0
+#     if arr[i] == target:
+#         return i
+#     i += 1
+#     return agnostic_binary_search(arr, target)
+    
